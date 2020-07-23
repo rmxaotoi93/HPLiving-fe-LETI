8c7ed10570bc7fc8ae8921a8e033a339e58ef675
rmxaotoi93 <cauchu2287>
2020-07-23 22:16:16 +0700

    final-fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,14 +5,27 @@ import axios from "axios";
 export default function Profile() {
   let { id } = useParams();
   let [user, setUser] = useState([]);
-
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_SERVER}/users/me/${id}`).then((res) => {
       console.log(res.data.data);
       setUser(res.data.data);
+      setEmail(res.data.data.email);
+      setName(res.data.data.name);
     });
   }, []);
-
+  const updateUser = async () => {
+    const oldUser = { name: name, email: email };
+    const newUser = await axios.put(
+      `${process.env.REACT_APP_SERVER}/users/${id}`,
+      oldUser
+    );
+    console.log("newuser", newUser);
+    setUser(newUser.data.data);
+    setEmail(newUser.data.data.email);
+    setName(newUser.data.data.name);
+  };
   return (
     <Container>
       <div className="profile">
@@ -32,12 +45,17 @@ export default function Profile() {
           <Col lg={3}>
             {" "}
             <div className="profile-title">
-              <h4>Title</h4>
+              <h4>User</h4>
             </div>
           </Col>
           <Col lg={9}>
             <div className="profile-title">
-              <input type="text" placeholder="Title" />
+              <input
+                type="text"
+                placeholder="Title"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
           </Col>
         </Row>
@@ -45,68 +63,22 @@ export default function Profile() {
           <Col lg={3}>
             {" "}
             <div className="profile-title">
-              <h4>Type of </h4>
+              <h4>Email </h4>
             </div>
           </Col>
           <Col lg={9}>
             <div className="profile-title">
-              <input type="text" placeholder="Type of" />
+              <input
+                type="text"
+                placeholder="Type of"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
           </Col>
         </Row>
-        <Row>
-          <Col lg={3}>
-            {" "}
-            <div className="profile-title">
-              <h4>Description</h4>
-            </div>
-          </Col>
-          <Col lg={9}>
-            <div className="profile-title">
-              <input type="text" placeholder="Detail information" />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={3}>
-            {" "}
-            <div className="profile-title">
-              <h4>Images</h4>
-            </div>
-          </Col>
-          <Col lg={9}>
-            <div className="profile-title">
-              <input type="text" placeholder="username" />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={3}>
-            {" "}
-            <div className="profile-title">
-              <h4>Price</h4>
-            </div>
-          </Col>
-          <Col lg={9}>
-            <div className="profile-title">
-              <input type="text" placeholder="$" />
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={3}>
-            {" "}
-            <div className="profile-title">
-              <h4>Location</h4>
-            </div>
-          </Col>
-          <Col lg={9}>
-            <div className="profile-title">
-              <input type="text" placeholder="Address" />
-            </div>
-          </Col>
-        </Row>
-        <Button>Update Profile</Button>
+
+        <Button onClick={updateUser}>Update Profile</Button>
       </div>
     </Container>
   );
